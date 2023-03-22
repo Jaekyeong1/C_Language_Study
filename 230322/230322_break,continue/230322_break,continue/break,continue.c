59644ddf632bfb742a0c681081588750a2ac464c
jaekyeong1 <kingjk0157@naver.com>
2023-03-22 21:22:46 +0900

    feat : break/continue 연습

---
+++ 230322/230322_break,continue/230322_break,continue/break,continue.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	/*
+	for (int i = 1; i <= 30; i++) {
+		if (i >= 6) {
+			printf("나머지 학생은 집에 가세요\n");
+			break;
+		}
+		printf("%d 번 학생은 조별 발표 준비를 하세요.\n");
+	}
+	for (int i = 1; i <= 30; i++) {
+		if (i >= 6 && i <= 10) {
+			if (i == 7) {
+				printf("%d 번 학생은 결석입니다\n", i);
+			}
+			printf("%d 번 학생은 조별 발표 준비를 하세요\n", i);
+		}
+	}
+	*/
+	return 0;
+}
